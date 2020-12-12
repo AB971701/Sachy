adb33b30adc517602f99bd77c5ef1c331754d68c
LuhiHofi <69843728+LuhiHofi@users.noreply.github.com>
2020-12-12 22:52:31 +0100

    Update Piece.py
    Added types of pieces

--- Piece.py
+++ Piece.py
@@ -4,3 +4,27 @@ class Piece:
 
     def __del__(self):
         pass
+
+class King(Piece):
+    pass
+
+
+class Queen(Piece):
+    pass
+
+
+class Tower(Piece):
+    pass
+
+
+class Bishop(Piece):
+    pass
+
+
+class Knight(Piece):
+    pass
+
+
+class Pawn(Piece):
+    pass
+
