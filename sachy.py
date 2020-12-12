3dce55d637aaa94cabe03a2d6316214b1f693618
LuhiHofi <69843728+LuhiHofi@users.noreply.github.com>
2020-12-12 21:43:38 +0100

    Add files via upload

---
+++ sachy.py
@@ -0,0 +1,26 @@
+class Chess:
+    def __init__(self):
+        #cteni a nacitani ze souboru
+        fen = open("chess_game.txt", "w+")
+
+    def __del__(self):
+        #body of destructor
+        pass
+
+    def read(self):
+        #read from fen file
+        pass
+
+    def write(self):
+        #write to fen file
+        pass
+
+    def Legit(self):
+        #check if move is legit
+        pass
+
+
+
+    """
+    Vlastnosti figurek    
+    """
