532b15e1c0614cc65d0aa9c7e4f873f31b63f6b2
LuhiHofi <69843728+LuhiHofi@users.noreply.github.com>
2021-01-24 20:33:36 +0100

    some changes and adition
    changed color, mouse click, combining both classes

--- ChessGUI.py
+++ ChessGUI.py
@@ -1,74 +1,102 @@
 import tkinter as tk
-
-"""
-I need the board list
-"""
+from Chess import Chess
 
 class ChessGUI:
-    def __init__(self):
-        root = tk.Tk()
-        canvas = tk.Canvas(bg='brown', width='900', height='900')
-        canvas.pack()
-        squares = self.__CreateBoard(canvas)
+    root = tk.Tk()
+    __previous_board = 0
+    pieces = None
+    squares = None
 
-        root.mainloop()
+    choices = {'Q': 'Images/w_queen.png', 'K': 'Images/w_king.png', 'B': 'Images/w_bishop.png',
+               'N': 'Images/w_knight.png', 'R': 'Images/w_rook.png', 'P': 'Images/w_pawn.png',
+               'q': 'Images/b_queen.png', 'k': 'Images/b_king.png', 'b': 'Images/b_bishop.png',
+               'n': 'Images/b_knight.png', 'r': 'Images/b_rook.png', 'p': 'Images/b_pawn.png'}
+
+    def __init__(self, board=None):
+        if board == None:
+            board_history = ['rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1']
+            tmp = (board_history[-1][0:board_history[-1].find(' ')]).split('/')
+            # vytvori prazdne herni pole
+            board = [[None for _ in range(8)] for _ in range(8)]
+            # polohu figur z posledniho tvaru zapise do herniho pole
+            for i in range(8):
+                j = 0
+                for c in tmp[i]:
+                    if c.lower() in "prnbqk":
+                        board[i][j] = c
+                        j += 1
+                    elif c in "12345678":
+                        j += ord(c) - ord('0')
+        self.__previous_board = board
+        canvas = tk.Canvas(bg='grey', width='900', height='900')
+        canvas.pack()
+        self.squares = self.__CreateBoard(canvas)
+        self.pieces = self.__PutPieces(canvas, board)
 
     def __del__(self):
         pass
+
+    def End(self):
+        self.root.mainloop()
 
     def __CreateBoard(self, canvas):
         squares = []
         for i in range(8):
             for k in range(8):
-                if ((i + k) % 2 == 0):
-                    squares.append(canvas.create_rectangle(50 + k * 100,
-                                                   50 + i * 100,
-                                                   150 + k * 100,
-                                                   150 + i * 100,
-                                                   fill='white'))
+                if (i + k) % 2 == 0:
+                    squares.append(canvas.create_rectangle(50 + i * 100,
+                                                           50 + k * 100,
+                                                           150 + i * 100,
+                                                           150 + k * 100,
+                                                           fill='pale goldenrod',
+                                                           outline=""))
                 else:
-                    squares.append(canvas.create_rectangle(50 + k * 100,
-                                            50 + i * 100,
-                                            150 + k * 100,
-                                            150 + i * 100,
-                                            fill='black'))
+                    squares.append(canvas.create_rectangle(50 + i * 100,
+                                                           50 + k * 100,
+                                                           150 + i * 100,
+                                                           150 + k * 100,
+                                                           fill='dark olive green',
+                                                           outline=""))
         pismena = "abcdefgh"
         for i in range(8):
             canvas.create_text((i + 1) * 100,
-                               int(canvas['height']) - 25,
+                               900 - 25,
                                text=pismena[i],
                                font='Arial',
                                fill='black')
             canvas.create_text(25,
-                               int(canvas['height']) - (i + 1) * 100,
+                               900 - (i + 1) * 100,
                                text=i + 1,
                                font='Arial',
                                fill='black')
         return squares
 
-    def PutPieces(self, canvas, board):
+    def __PutPieces(self, canvas, board):
         pieces = []
-        #qkbnrp QKBNRP
-        choices = {'Q': 'Images/w_queen.png', 'K': 'Images/w_king.png', 'B': 'Images/w_bishop.png',
-                   'N': 'Images/w_knight.png', 'R': 'Images/w_rook.png', 'P': 'Images/w_pawn.png',
-                   'q': 'Images/b_queen.png', 'k': 'Images/b_king.png', 'b': 'Images/b_bishop.png',
-                   'n': 'Images/b_knight.png', 'r': 'Images/b_rook.png', 'p': 'Images/b_pawn.png',
-                   '-': }
-        for square in board:
-            if square in choices:
-                result = choices.get(square)
-        """
-        bk = Image.open("Images/black_king.png")
-        bk = bk.resize((50, 50), Image.ANTIALIAS)
-        bk = ImageTk.PhotoImage(bk)
-        black_king = canvas.create_image(0,0, anchor='nw', image=bk)
-        pieces.append(black_king)
+        for line in range(len(board)):
+            for square in range(len(board[line])):
+                if board[line][square] in self.choices:
+                    piece = tk.PhotoImage(file=self.choices.get(board[line][square]))
+                    pieces.append(piece)
+                    canvas.create_image(55 + square * 100, 55 + line * 100, anchor='nw', image=piece)
+                else:
+                    pieces.append(None)
         return pieces
-        """
 
-
-def main():
-    play = ChessGUI()
-
-if __name__ == "__main__":
-    main()
+    def AfterMove(self, canvas, board):
+        if self.__previous_board != board:
+            for line in range(len(board)):
+                for square in range(len(board[line])):
+                    if board[line][square] != self.__previous_board[line][square]:
+                        if self.pieces[line * 8 + square] != None:
+                            canvas.delete(self.pieces[line * 8 + square])
+                            if board[line][square] == None:
+                                self.pieces[line * 8 + square] = None
+                            else:
+                                self.pieces[line * 8 + square] = tk.PhotoImage(file=self.choices.get(board[line][square]))
+                                canvas.create_image(55 + square * 100, 55 + line * 100, anchor='nw', image=self.pieces[line * 8 + square])
+                        else:
+                            self.pieces[line * 8 + square] = tk.PhotoImage(file=self.choices.get(board[line][square]))
+                            canvas.create_image(55 + square * 100, 55 + line * 100, anchor='nw',
+                                                image=self.pieces[line * 8 + square])
+        self.__previous_board = board
--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-import Chess
+from Chess import Chess
+from ChessGUI import ChessGUI
+
+def play_from_file(filepath):
+    c = Chess()
+    with open(filepath) as file:
+        moves = [(move.strip()).split(',') for move in file]
+    for move in moves:
+        print('white plays' if c.white_plays else 'black plays')
+        if c.move(move[0], move[1]):
+            c.print_board()
+            print()
+        else:
+            print('invalid move')
+            break
+    c.save_to_file("save.txt")
+
+def callback(event):
+    INDEX_TO_LETTER = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h"}
+    NUMBER_TO_INDEX = {0: 8, 1: 7, 2: 6, 3: 5, 4: 4, 5: 3, 6: 2, 7: 1}
+    print(INDEX_TO_LETTER[int((event.x - 50 )/ 100)] + str(NUMBER_TO_INDEX[int((event.y - 50 )/ 100)]))
+    # Chess.Chess.move(, INDEX_TO_LETTER[int(event.x) / 100 - 50] + NUMBER_TO_INDEX[int(event.y) / 100 - 50])
+
+
+def main():
+    chess = Chess()
+    gui = ChessGUI()
+    gui.root.bind("<Button-1>", callback)
+    gui.End()
+
+if __name__ == "__main__":
+    main()
